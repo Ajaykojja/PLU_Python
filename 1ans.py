0c19a64ff407494c0e95b49ba727432e07e8c0e6
ajay <ajaykojja63@gmail.com>
2026-06-30 11:17:19 +0530

    test1

---
+++ 1ans.py
@@ -0,0 +1,7 @@
+a = int(input("Enter integer: "))
+b = float(input("Enter float: "))
+c = input("Enter string: ")
+
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ 2ans.py
@@ -0,0 +1,8 @@
+a = (input("Enter a value: "))
+b = (input("Enter another value: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
